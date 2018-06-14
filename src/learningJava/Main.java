1342dd48c7872d57349cfb3227f9c1c103390d71
Jagadesha H <jagadesha.h@compassitesinc.com>
2018-06-14 22:44:31 +0530

    added a brief description about oop

--- src/learningJava/Main.java
+++ src/learningJava/Main.java
@@ -15,5 +15,7 @@ public class Main {
 		// main is function and it is a starting point for java programs
 		// the following line of code prints Hello, Java! to output device.
 		System.out.println("Hello, Java!");
-	}	
+	}
+	// in objected oriented programming programs always have atleast one class and 
+	// one main method
 }
