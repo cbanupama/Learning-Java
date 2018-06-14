dd33c9a0f1de2f95654535689d510567e12bb5ef
Anupama CB <cbanupama03@gmail.com>
2018-06-14 22:36:48 +0530

    Creat a Project

---
+++ src/learningJava/Main.java
@@ -0,0 +1,19 @@
+package learningJava;
+
+public class Main {	
+	// This is a comment where we can document about our code
+	// These are single line comments
+
+	/*
+	 * This is multiple line comments
+	 * 
+	 * Java is an object oriented programming language
+	 * meaning everything in java is an object
+	 */
+	
+	public static void main (String args[]) {
+		// main is function and it is a starting point for java programs
+		// the following line of code prints Hello, Java! to output device.
+		System.out.println("Hello, Java!");
+	}	
+}
